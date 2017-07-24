7b4811e2f141f48cc6e07c1e2d8c66042c070deb
Harry Krewson <harrykrewson@gmail.com>
2017-07-24 13:29:30 -0600

    made the receipt more secure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721221832) do
+ActiveRecord::Schema.define(version: 20170724192354) do
 
   create_table "purchases", force: :cascade do |t|
     t.string "email"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170721221832) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uuid"
   end
 
 end
